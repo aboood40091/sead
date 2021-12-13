b055f78c5bc7644bdb0fced76e4a64b37dbcef3d
AboodXD <aboood40091@gmail.com>
2021-12-13 15:29:57 +0400

    math/GraphicsContext: Implement apply()

--- modules/src/gfx/seadGraphicsContext.cpp
+++ modules/src/gfx/seadGraphicsContext.cpp
@@ -45,4 +45,93 @@ GraphicsContext::GraphicsContext()
 {
 }
 
+void GraphicsContext::apply() const
+{
+#ifdef cafe
+    GX2SetDepthStencilControl(
+        static_cast<GX2Boolean>(mDepthTestEnable),
+        static_cast<GX2Boolean>(mDepthWriteEnable),
+        static_cast<GX2CompareFunction>(mDepthFunc),
+        static_cast<GX2Boolean>(mStencilTestEnable),
+        static_cast<GX2Boolean>(mStencilTestEnable),
+        static_cast<GX2CompareFunction>(mStencilTestFunc),
+        static_cast<GX2StencilFunction>(mStencilOpZPass),
+        static_cast<GX2StencilFunction>(mStencilOpZFail),
+        static_cast<GX2StencilFunction>(mStencilOpFail),
+        static_cast<GX2CompareFunction>(mStencilTestFunc),
+        static_cast<GX2StencilFunction>(mStencilOpZPass),
+        static_cast<GX2StencilFunction>(mStencilOpZFail),
+        static_cast<GX2StencilFunction>(mStencilOpFail)
+    );
+
+    GX2SetStencilMask(mStencilTestMask, mStencilTestRef, mStencilTestRef,  // Bug: ref in place of write mask
+                      mStencilTestMask, mStencilTestRef, mStencilTestRef); // ^^^
+
+    {
+        static const GX2Boolean cCullSettings[4][2] = {
+            { GX2_ENABLE, GX2_DISABLE },
+            { GX2_DISABLE, GX2_ENABLE },
+            { GX2_DISABLE, GX2_DISABLE },
+            { GX2_ENABLE, GX2_ENABLE }
+        };
+        const GX2Boolean* const& settings = cCullSettings[mCullingMode];
+
+        GX2SetPolygonControl(
+            GX2_FRONT_FACE_CCW,
+            settings[0],
+            settings[1],
+            static_cast<GX2Boolean>(mPolygonModeFront != Graphics::cPolygonMode_Fill || mPolygonModeBack != Graphics::cPolygonMode_Fill),
+            static_cast<GX2PolygonMode>(mPolygonModeFront),
+            static_cast<GX2PolygonMode>(mPolygonModeBack),
+            static_cast<GX2Boolean>(mPolygonOffsetFrontEnable),
+            static_cast<GX2Boolean>(mPolygonOffsetBackEnable),
+            static_cast<GX2Boolean>(mPolygonOffsetPointLineEnable)
+        );
+    }
+
+    GX2SetColorControl(
+        GX2_LOGIC_OP_NONE,
+        mBlendEnable ? 0xFF : 0,
+        GX2_DISABLE,
+        static_cast<GX2Boolean>(mColorMaskR || mColorMaskG || mColorMaskB || mColorMaskA)
+    );
+
+    GX2SetBlendControl(
+        GX2_RENDER_TARGET_0,
+        static_cast<GX2BlendFunction>(mBlendFactorSrcRGB),
+        static_cast<GX2BlendFunction>(mBlendFactorDstRGB),
+        static_cast<GX2BlendCombine>(mBlendEquationRGB),
+        GX2_ENABLE,
+        static_cast<GX2BlendFunction>(mBlendFactorSrcA),
+        static_cast<GX2BlendFunction>(mBlendFactorDstA),
+        static_cast<GX2BlendCombine>(mBlendEquationA)
+    );
+
+    GX2SetBlendConstantColor(mBlendConstantColor.r,
+                             mBlendConstantColor.g,
+                             mBlendConstantColor.b,
+                             mBlendConstantColor.a);
+
+    GX2SetAlphaTest(static_cast<GX2Boolean>(mAlphaTestEnable),
+                    static_cast<GX2CompareFunction>(mAlphaTestFunc),
+                    mAlphaTestRef);
+
+    GX2SetTargetChannelMasks(
+        static_cast<GX2ChannelMask>(mColorMaskR * GX2_CHANNEL_MASK_R +
+                                    mColorMaskG * GX2_CHANNEL_MASK_G +
+                                    mColorMaskB * GX2_CHANNEL_MASK_B +
+                                    mColorMaskA * GX2_CHANNEL_MASK_A),
+        GX2_CHANNEL_MASK_NONE,
+        GX2_CHANNEL_MASK_NONE,
+        GX2_CHANNEL_MASK_NONE,
+        GX2_CHANNEL_MASK_NONE,
+        GX2_CHANNEL_MASK_NONE,
+        GX2_CHANNEL_MASK_NONE,
+        GX2_CHANNEL_MASK_NONE
+    );
+
+    GX2SetAlphaToMask(GX2_DISABLE, GX2_ALPHA_TO_MASK_0);
+#endif // cafe
+}
+
 } // namespace sead
