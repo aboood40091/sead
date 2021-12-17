86ed46bd3d9e077db8f0d6d955bdf809e9299162
AboodXD <aboood40091@gmail.com>
2021-12-17 18:33:30 +0400

    gfx: Update Projection

--- include/gfx/seadProjection.h
+++ include/gfx/seadProjection.h
@@ -8,22 +8,50 @@
 
 namespace sead {
 
+class Camera;
+class Viewport;
+
+template <typename T>
+class Ray;
+
 class Projection
 {
     SEAD_RTTI_BASE(Projection)
 
 public:
+    enum Type
+    {
+        cType_Perspective = 0,
+        cType_Ortho       = 1,
+        cType_Undefined   = 2
+    };
+
+public:
     Projection();
     virtual ~Projection();
 
-    virtual u32 getProjectionType() const = 0;
-    virtual void doUpdateMatrix(Matrix44f* mtx) const = 0;
-    virtual void doUpdateDeviceMatrix(Matrix44f*, const Matrix44f&, Graphics::DevicePosture) const;
-    virtual void doScreenPosToCameraPosTo(Vector3f*, const Vector3f&) const = 0;
+    virtual Type getProjectionType() const = 0;
+    virtual void doUpdateMatrix(Matrix44f* dst) const = 0;
+    virtual void doUpdateDeviceMatrix(Matrix44f* dst, const Matrix44f& src, Graphics::DevicePosture pose) const;
+    virtual void doScreenPosToCameraPosTo(Vector3f* dst, const Vector3f& screen_pos) const = 0;
 
-    void updateMatrixImpl_() const;
+    const Matrix44f& getProjectionMatrix() const;
+    Matrix44f& getProjectionMatrixMutable();
+
     const Matrix44f& getDeviceProjectionMatrix() const;
 
+    void cameraPosToScreenPos(Vector3f* dst, const Vector3f& camera_pos) const;
+    void screenPosToCameraPos(Vector3f* dst, const Vector3f& screen_pos) const;
+    void screenPosToCameraPos(Vector3f* dst, const Vector2f& screen_pos) const;
+
+    void project(Vector2f* dst, const Vector3f& camera_pos, const Viewport& viewport) const;
+    void unproject(Vector3f* dst, const Vector3f& screen_pos, const Camera& camera) const;
+    void unprojectRay(Ray<Vector3f>* dst, const Vector3f& screen_pos, const Camera& camera) const;
+
+private:
+    void updateMatrixImpl_() const;
+
+protected:
     mutable bool mDirty;
     mutable bool mDeviceDirty;
     Matrix44f mMatrix;
