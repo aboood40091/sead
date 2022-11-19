061b332c36473572f7b123c3068ec0ffa7685f81
AboodXD <aboood40091@gmail.com>
2022-11-19 16:08:58 +0400

    gfx: Add GraphicsContextMRT

---
+++ include/gfx/seadGraphicsContextMRT.h
@@ -0,0 +1,78 @@
+#ifndef SEAD_GRAPHICS_CONTEXT_MRT_H_
+#define SEAD_GRAPHICS_CONTEXT_MRT_H_
+
+#include <gfx/seadColor.h>
+#include <gfx/seadGraphics.h>
+
+namespace sead {
+
+class GraphicsContextMRT
+{
+    struct BlendExpression
+    {
+        BlendExpression()
+            : blend_factor_src_rgb(Graphics::cBlendFactor_SrcAlpha)
+            , blend_factor_src_a(Graphics::cBlendFactor_SrcAlpha)
+            , blend_factor_dst_rgb(Graphics::cBlendFactor_InvSrcAlpha)
+            , blend_factor_dst_a(Graphics::cBlendFactor_InvSrcAlpha)
+            , blend_equation_rgb(Graphics::cBlendEquation_Add)
+            , blend_equation_a(Graphics::cBlendEquation_Add)
+        {
+        }
+
+        Graphics::BlendFactor blend_factor_src_rgb;
+        Graphics::BlendFactor blend_factor_src_a;
+        Graphics::BlendFactor blend_factor_dst_rgb;
+        Graphics::BlendFactor blend_factor_dst_a;
+        Graphics::BlendEquation blend_equation_rgb;
+        Graphics::BlendEquation blend_equation_a;
+    };
+
+public:
+    GraphicsContextMRT();
+    virtual ~GraphicsContextMRT()
+    {
+    }
+
+    void apply() const;
+    void applyAlphaTest() const;
+    void applyDepthAndStencilTest() const;
+    void applyColorMask() const;
+    void applyBlendAndFastZ() const;
+    void applyBlendConstantColor() const;
+    void applyCullingAndPolygonModeAndPolygonOffset() const;
+
+//private:
+    bool mDepthTestEnable;
+    bool mDepthWriteEnable;
+    Graphics::DepthFunc mDepthFunc;
+    Graphics::CullingMode mCullingMode;
+    sead::BitFlag32 mBlendEnableMask;
+    BlendExpression mBlendExpression[8];
+    Color4f mBlendConstantColor;
+    bool mAlphaTestEnable;
+    Graphics::AlphaFunc mAlphaTestFunc;
+    f32 mAlphaTestRef;
+    u32 mColorMask;
+    bool mStencilTestEnable;
+    Graphics::StencilFunc mStencilTestFunc;
+    s32 mStencilTestRef;
+    u32 mStencilTestMask;
+    Graphics::StencilOp mStencilOpFail;
+    Graphics::StencilOp mStencilOpZFail;
+    Graphics::StencilOp mStencilOpZPass;
+#ifdef cafe
+    Graphics::PolygonMode mPolygonModeFront;
+    Graphics::PolygonMode mPolygonModeBack;
+    bool mPolygonOffsetFrontEnable;
+    bool mPolygonOffsetBackEnable;
+    bool mPolygonOffsetPointLineEnable;
+#endif // cafe
+};
+#ifdef cafe
+static_assert(sizeof(GraphicsContextMRT) == 0x11C, "sead::GraphicsContextMRT size mismatch");
+#endif // cafe
+
+} // namespace sead
+
+#endif // SEAD_GRAPHICS_CONTEXT_MRT_H_
