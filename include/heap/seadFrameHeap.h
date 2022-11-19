09c04284277915dfa8daaded16ea7cb5d852c86b
AboodXD <aboood40091@gmail.com>
2022-11-19 16:09:09 +0400

    heap: Add FrameHeap

---
+++ include/heap/seadFrameHeap.h
@@ -0,0 +1,53 @@
+#ifndef SEAD_FRAME_HEAP_H_
+#define SEAD_FRAME_HEAP_H_
+
+#include <heap/seadHeap.h>
+
+namespace sead {
+
+class FrameHeap : public Heap
+{
+    SEAD_RTTI_OVERRIDE(FrameHeap, Heap)
+
+public:
+    FrameHeap(const SafeString& name, Heap* parent, void* start, u32 size, HeapDirection direction, bool enable_lock);
+    virtual ~FrameHeap();
+
+    virtual void destroy();
+    virtual u32 adjust();
+    virtual void* tryAlloc(size_t size, s32 alignment);
+    virtual void free(void* ptr);
+    virtual void* resizeFront(void*, size_t);
+    virtual void* resizeBack(void*, size_t);
+    virtual void freeAll();
+    virtual u32 getStartAddress() const;
+    virtual u32 getEndAddress() const;
+    virtual u32 getSize() const;
+    virtual u32 getFreeSize() const;
+    virtual u32 getMaxAllocatableSize(int) const;
+    virtual bool isInclude(const void*) const;
+    virtual bool isFreeable() const;
+    virtual bool isResizable() const;
+    virtual bool isAdjustable() const;
+
+public:
+    static FrameHeap* tryCreate(u32 size, const SafeString& name, Heap* parent = NULL, HeapDirection direction = cHeapDirection_Forward, bool enable_lock = false);
+
+protected:
+    class State
+    {
+        void* mHeadPtr;
+        void* mTailPtr;
+
+        friend class FrameHeap;
+    };
+
+    State state;
+};
+#ifdef cafe
+static_assert(sizeof(FrameHeap) == 0x98, "sead::FrameHeap size mismatch");
+#endif // cafe
+
+} // namespace sead
+
+#endif // SEAD_FRAME_HEAP_H_
