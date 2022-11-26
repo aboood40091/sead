3c674ac943c6dd9f1caaae6bcc849dd2ef2d90fe
AboodXD <aboood40091@gmail.com>
2022-11-26 13:26:28 +0400

    gfx/GraphicsContextMRT: Implement ctor

--- include/gfx/seadGraphicsContextMRT.h
+++ include/gfx/seadGraphicsContextMRT.h
@@ -3,6 +3,7 @@
 
 #include <gfx/seadColor.h>
 #include <gfx/seadGraphics.h>
+#include <prim/seadBitFlag.h>
 
 namespace sead {
 
@@ -48,7 +49,7 @@ public:
     Graphics::DepthFunc mDepthFunc;
     Graphics::CullingMode mCullingMode;
     sead::BitFlag32 mBlendEnableMask;
-    BlendExpression mBlendExpression[8];
+    BlendExpression mBlendExpression[8]; // sead::UnsafeArray<BlendExpression, 8>
     Color4f mBlendConstantColor;
     bool mAlphaTestEnable;
     Graphics::AlphaFunc mAlphaTestFunc;
---
+++ modules/src/gfx/seadGraphicsContextMRT.cpp
@@ -0,0 +1,34 @@
+#include <gfx/seadGraphicsContextMRT.h>
+
+namespace sead {
+
+GraphicsContextMRT::GraphicsContextMRT()
+    : mDepthTestEnable(true)
+    , mDepthWriteEnable(true)
+    , mDepthFunc(Graphics::cDepthFunc_LessEqual)
+    , mCullingMode(Graphics::cCullingMode_Back)
+    , mBlendEnableMask(0xFF)
+    , mBlendExpression()
+    , mBlendConstantColor(1.0f, 1.0f, 1.0f, 1.0f)
+    , mAlphaTestEnable(false)
+    , mAlphaTestFunc(Graphics::cAlphaFunc_Greater)
+    , mAlphaTestRef(0.0f)
+    , mColorMask(0xFFFFFFFF)
+    , mStencilTestEnable(false)
+    , mStencilTestFunc(Graphics::cStencilFunc_Never)
+    , mStencilTestRef(0)
+    , mStencilTestMask(0xFFFFFFFF)
+    , mStencilOpFail(Graphics::cStencilOp_Keep)
+    , mStencilOpZFail(Graphics::cStencilOp_Keep)
+    , mStencilOpZPass(Graphics::cStencilOp_Keep)
+#ifdef cafe
+    , mPolygonModeFront(Graphics::cPolygonMode_Fill)
+    , mPolygonModeBack(Graphics::cPolygonMode_Fill)
+    , mPolygonOffsetFrontEnable(false)
+    , mPolygonOffsetBackEnable(false)
+    , mPolygonOffsetPointLineEnable(false)
+#endif // cafe
+{
+}
+
+} // namespace sead
