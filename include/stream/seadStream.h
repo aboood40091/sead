8ae097f60bf9597adab77945cd25d84ff6cd1744
AboodXD <aboood40091@gmail.com>
2023-01-05 15:20:12 +0400

    stream: Add Stream and ReadStream

---
+++ include/stream/seadStream.h
@@ -0,0 +1,73 @@
+#ifndef SEAD_STREAM_H_
+#define SEAD_STREAM_H_
+
+#include <prim/seadEndian.h>
+#include <prim/seadSafeString.h>
+
+namespace sead {
+
+class StreamFormat;
+class StreamSrc;
+
+class Stream
+{
+public:
+    Stream();
+    virtual ~Stream() { }
+
+protected:
+    StreamFormat* mFormat;
+    StreamSrc* mSrcStream;
+    Endian::Types mSrcEndian;
+};
+
+class ReadStream : public Stream
+{
+public:
+    ReadStream()
+    {
+    }
+
+    virtual ~ReadStream()
+    {
+    }
+
+    u8 readU8();
+    u16 readU16();
+    u32 readU32();
+    u64 readU64();
+
+    s8 readS8();
+    s16 readS16();
+    s32 readS32();
+    s64 readS64();
+
+    f32 readF32();
+
+    void readU8(u8&);
+    void readU16(u16&);
+    void readU32(u32&);
+    void readU64(u64&);
+
+    void readS8(s8&);
+    void readS16(s16&);
+    void readS32(s32&);
+    void readS64(s64&);
+
+    void readF32(f32&);
+
+    void readBit(void*, u32);
+    void readString(BufferedSafeString* dst, u32 size);
+    void readMemBlock(void*, u32);
+
+protected:
+    f32 readF32BitImpl_(u32 exp_bitnum, u32 significand_bitnum);
+    f32 readF64BitImpl_(u32 exp_bitnum, u32 significand_bitnum);
+
+    static const u32 cExpBitMax = 8;
+    static const u32 cSignficandBitMax = 23;
+};
+
+} // namespace sead
+
+#endif // SEAD_STREAM_H_
