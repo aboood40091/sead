dd9fa063fde54de19b31bbffb5335a4e3d7e04d9
AboodXD <aboood40091@gmail.com>
2023-01-13 16:49:27 +0400

    engine/prim/Delegate: Add missing functions and typedefs

--- engine/library/include/prim/seadDelegate.h
+++ engine/library/include/prim/seadDelegate.h
@@ -1,16 +1,34 @@
 #ifndef SEAD_DELEGATE_H_
 #define SEAD_DELEGATE_H_
 
-#include <basis/seadTypes.h>
+#include <basis/seadNew.h>
 
 namespace sead {
 
-class Heap;
+template <typename T, typename R>
+class DelegateTraits
+{
+public:
+    typedef R (T::*MethodPtr)();
+};
+
+template <typename T, typename A, typename R>
+class DelegateTraits1
+{
+public:
+    typedef R (T::*MethodPtr)(A);
+};
 
 class IDelegate
 {
 public:
     virtual void invoke() = 0;
+
+    void operator() ()
+    {
+        return invoke();
+    }
+
     virtual IDelegate* clone(Heap*) const = 0;
 };
 
@@ -19,11 +37,23 @@ class IDelegate1
 {
 public:
     virtual void invoke(A a) = 0;
+
+    void operator() (A a)
+    {
+        return Invoke(a);
+    }
 };
 
-template <typename T, typename PTMF, typename Base>
-class DelegateBase : public Base
+template <typename T, typename METHODPTR, typename IDELEGATE>
+class DelegateBase : public IDELEGATE
 {
+private:
+    typedef METHODPTR MethodPtr;
+
+protected:
+    T* mObject;
+    METHODPTR mMethod;
+
 public:
     DelegateBase()
         : mObject(nullptr)
@@ -31,75 +61,113 @@ public:
     {
     }
 
-    DelegateBase(T* o, PTMF m)
+    DelegateBase(T* o, METHODPTR m)
         : mObject(o)
         , mMethod(m)
     {
     }
 
-    void bind(T* o, PTMF m)
+    void bind(T* o, METHODPTR m)
     {
         mObject = o;
         mMethod = m;
     }
 
-protected:
-    T* mObject;
-    PTMF mMethod;
+    void bind(T* o)
+    {
+        mObject = o;
+    }
+
+    void bind(METHODPTR m)
+    {
+        mMethod = m;
+    }
+
+    void unbind()
+    {
+        mObject = nullptr;
+        mMethod = nullptr;
+    }
+
+    T* getObject() const
+    {
+        return mObject;
+    }
 };
 
 template <typename T>
-class Delegate : public DelegateBase< T, void (T::*)(), IDelegate >
+class Delegate : public DelegateBase< T, typename DelegateTraits<T, void>::MethodPtr, IDelegate >
 {
 public:
-    typedef void (T::*MethodPtr)();
-    typedef DelegateBase< T, MethodPtr, IDelegate > Base;
+    typedef Delegate<T> self;
+    typedef typename DelegateTraits<T, void>::MethodPtr MethodPtr;
 
 public:
     Delegate()
-        : Base()
+        : DelegateBase< T, MethodPtr, IDelegate >()
     {
     }
 
     Delegate(T* o, MethodPtr m)
-        : Base(o, m)
+        : DelegateBase< T, MethodPtr, IDelegate >(o, m)
     {
     }
 
     virtual void invoke()
     {
         if (mObject && mMethod)
-            return (mObject->*(mMethod))();
+            (mObject->*mMethod)();
+    }
+
+    void invoke() const
+    {
+        if (mObject && mMethod)
+            (mObject->*mMethod)();
+    }
+
+    void operator() () const
+    {
+        invoke();
     }
 
     virtual IDelegate* clone(Heap* heap) const
     {
-        return new (heap) Delegate(*this);
+        return new (heap) self(*this);
     }
 };
 
 template <typename T, typename A>
-class Delegate1 : public DelegateBase< T, void (T::*)(A), IDelegate1<A> >
+class Delegate1 : public DelegateBase< T, typename DelegateTraits1<T, A, void>::MethodPtr, IDelegate1<A> >
 {
 public:
-    typedef void (T::*MethodPtr)(A);
-    typedef DelegateBase< T, MethodPtr, IDelegate1<A> > Base;
+    typedef typename DelegateTraits1<T, A, void>::MethodPtr MethodPtr;
 
 public:
     Delegate1()
-        : Base()
+        : DelegateBase< T, MethodPtr, IDelegate1<A> >()
     {
     }
 
     Delegate1(T* o, MethodPtr m)
-        : Base(o, m)
+        : DelegateBase< T, MethodPtr, IDelegate1<A> >(o, m)
     {
     }
 
     virtual void invoke(A a)
     {
         if (mObject && mMethod)
-            return (mObject->*(mMethod))(a);
+            (mObject->*mMethod)(a);
+    }
+
+    void invoke(A a) const
+    {
+        if (mObject && mMethod)
+            (mObject->*mMethod)(a);
+    }
+
+    void operator() (A a) const
+    {
+        invoke(a);
     }
 };
 
