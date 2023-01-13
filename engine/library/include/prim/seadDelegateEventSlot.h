22f2e2954596603d6c256052758e075da293418c
AboodXD <aboood40091@gmail.com>
2023-01-13 18:05:11 +0400

    engine/prim/DelegateEventSlot: Complete structures

--- engine/library/include/prim/seadDelegateEventSlot.h
+++ engine/library/include/prim/seadDelegateEventSlot.h
@@ -3,12 +3,9 @@
 
 #include <container/seadTList.h>
 #include <heap/seadDisposer.h>
-//#include <prim/seadDelegate.h>
+#include <prim/seadDelegate.h>
 
 namespace sead {
-
-template <typename T>
-class IDelegate1;
 
 template <typename T>
 class DelegateEvent
@@ -23,21 +20,42 @@ private:
 public:
     class Slot : public IDisposer
     {
+    protected:
+        SlotListNode mNode;
+        IDelegate1<T>* mDelegatePtr;
+        struct { u8 data_[20]; } mDelegate;
+        bool mConnectedToDelegateEvent;
+
+        friend class DelegateEvent;
+
     public:
+        explicit Slot()
+            : IDisposer()
+            , m_Node()
+            , mDelegatePtr(nullptr)
+            , mConnectedToDelegateEvent(false)
+        {
+        }
+
+        virtual ~Slot()
+        {
+            release();
+        }
+
         template <typename U>
-        Slot(U*, void (U::*)(T)); /*
+        explicit Slot(U* u, void (U::*fun)(T))
             : IDisposer()
             , mNode()
             , mDelegatePtr(nullptr)
             , mConnectedToDelegateEvent(false)
         {
-            mDelegatePtr = new (mDelegate) Delegate1<U, T>();
+            mDelegatePtr = new (&mDelegate) Delegate1<U, T>(u, fun);
         }
-        */
 
-        virtual ~Slot()
+        template <typename U>
+        void bind(U* u, void (U::*fun)(T))
         {
-            release();
+            mDelegatePtr = new (&mDelegate) Delegate1<U, T>(u, fun);
         }
 
         void release()
@@ -49,30 +67,74 @@ public:
             }
         }
 
-        SlotListNode mNode;
-        IDelegate1<T>* mDelegatePtr;
-        struct { u8 data_[20]; } mDelegate;
-        bool mConnectedToDelegateEvent;
+        bool isConnectedToDelegateEvent() const
+        {
+            return mConnectedToDelegateEvent;
+        }
+
+    protected:
+        void invoke_(T e)
+        {
+            if (mDelegatePtr)
+                mDelegatePtr->invoke(e);
+        }
     };
 
 public:
-    DelegateEvent()
+    explicit DelegateEvent()
         : mList()
     {
     }
 
     virtual ~DelegateEvent()
     {
-        for (SlotList::iterator it = mList.begin(); it != mList.end(); ++it)
-            (*it)->release();
+        for (typename SlotList::iterator it = mList.begin(); it != mList.end(); )
+        {
+            Slot* s = *it;
+            ++it;
+            s->release();
+        }
     }
 
-    void connect(Slot&);
-    void disconnect(Slot&);
-    DelegateEvent<T>& operator+=(Slot&);
-    DelegateEvent<T>& operator-=(Slot&);
-    void fire(T);
-    s32 getSlotLength() const;
+    void connect(Slot& slot)
+    {
+        if (slot.mConnectedToDelegateEvent)
+            slot.release();
+
+        mList.pushBack(slot);
+        slot.mConnectedToDelegateEvent = true;
+    }
+
+    void disconnect(Slot& slot)
+    {
+        if (!slot.mConnectedToDelegateEvent)
+            return;
+
+        slot.release();
+    }
+
+    DelegateEvent<T>& operator+=(Slot& slot)
+    {
+        connect(slot);
+        return *this;
+    }
+
+    DelegateEvent<T>& operator-=(Slot& slot)
+    {
+        disconnect(slot);
+        return *this;
+    }
+
+    void fire(T e)
+    {
+        for (typename SlotList::iterator it = mList.begin(); it != mList.end(); ++it)
+            (*it)->invoke_(e);
+    }
+
+    s32 getSlotLength() const
+    {
+        return mList.size();
+    }
 
 protected:
     SlotList mList;
