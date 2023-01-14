6ac9512c76078f4af08ea73586ddd49a8fe2601c
AboodXD <aboood40091@gmail.com>
2023-01-14 16:06:14 +0400

    engine/container/FreeList: Complete definition

--- engine/library/include/container/seadFreeList.h
+++ engine/library/include/container/seadFreeList.h
@@ -5,25 +5,27 @@
 
 namespace sead {
 
-// TODO
-
 class FreeList
 {
 public:
-    FreeList();
+    FreeList()
+        : mFree(nullptr)
+        , mWork(nullptr)
+    {
+    }
 
     void init(void* work, s32 elem_size, s32 num)
     {
         // SEAD_ASSERT(work);
-        // SEAD_ASSERT(elem_size > 0 && elem_size % cPtrSize == 0);
+        // SEAD_ASSERT(elem_size > 0 && elem_size % 4 == 0);
         // SEAD_ASSERT(num > 0);
+
+        const s32 idx_multiplier = elem_size / 4;
 
         mFree = work;
 
-        const s32 idx_multiplier = elem_size / cPtrSize;
-
         for (s32 i = 0; i < num - 1; ++i)
-            static_cast<void**>(work)[i * idx_multiplier] = &static_cast<void**>(work)[(i + 1) * idx_multiplier];
+            static_cast<void**>(work)[idx_multiplier * i] = &static_cast<void**>(work)[idx_multiplier * (i + 1)];
 
         static_cast<void**>(work)[(num - 1) * idx_multiplier] = nullptr;
 
@@ -46,13 +48,9 @@ public:
         mWork = nullptr;
     }
 
-    void* work() { return mWork; }
-    const void* work() const { return mWork; }
+    void* work() const { return mWork; }
 
-    void* free() { return mFree; }
-    const void* free() const { return mFree; }
-
-    void free(void* ptr)
+    void put(void* ptr)
     {
         *static_cast<void**>(ptr) = mFree;
         mFree = ptr;
@@ -61,8 +59,6 @@ public:
 private:
     void* mFree;
     void* mWork;
-
-    static const size_t cPtrSize = sizeof(void*);
 };
 
 } // namespace sead
