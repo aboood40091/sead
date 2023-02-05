8d770a231792f8c79b512e0244a3320484913546
AboodXD <aboood40091@gmail.com>
2023-02-05 10:18:39 +0400

    agl/layer/RenderStep: Implement pushBack()

--- packages/agl/src/layer/aglRenderStep.cpp
+++ packages/agl/src/layer/aglRenderStep.cpp
@@ -2,6 +2,17 @@
 
 namespace agl { namespace lyr {
 
+bool RenderStep::pushBack(DrawMethod* p_method)
+{
+    // SEAD_ASSERT(p_method != nullptr);
+    for (sead::FixedPtrArray<DrawMethod, 256>::iterator itr = mpDrawMethod.begin(); itr != mpDrawMethod.end(); ++itr)
+        if (&(*itr) == p_method)
+            return false;
+
+    mpDrawMethod.pushBack(p_method);
+    return true;
+}
+
 s32 RenderStep::remove(const DrawMethod* p_method)
 {
     // SEAD_ASSERT(p_method != nullptr);
