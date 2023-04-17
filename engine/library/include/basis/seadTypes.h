64035e724d118dff390d4be302077286516e7c18
AboodXD <aboood40091@gmail.com>
2023-04-17 18:16:04 +0400

    Define override and forceinline

--- engine/library/include/basis/seadTypes.h
+++ engine/library/include/basis/seadTypes.h
@@ -13,22 +13,30 @@ typedef s64 d_sintptr;
 typedef uintptr uintptr_t;
 typedef sintptr  intptr_t;
 
+#ifndef override
+    #define override
+#endif // override
+
+#ifndef forceinline
+    #define forceinline __attribute__((always_inline))
+#endif // forceinline
+
 #ifndef nullptr
-#define nullptr NULL
+    #define nullptr NULL
 #endif // nullptr
 
 #ifndef static_assert
+    // https://stackoverflow.com/a/1597129
+    #define TOKENPASTE(x, y) x ## y
+    #define TOKENPASTE2(x, y) TOKENPASTE(x, y)
 
-// https://stackoverflow.com/a/1597129
-#define TOKENPASTE(x, y) x ## y
-#define TOKENPASTE2(x, y) TOKENPASTE(x, y)
-
-#define static_assert(condition, ...) typedef int TOKENPASTE2(static_assert_, __LINE__)[(condition) ? 1 : -1]
-
+    #define static_assert(condition, ...) typedef int TOKENPASTE2(static_assert_, __LINE__)[(condition) ? 1 : -1]
 #endif // static_assert
 
 #else
+
 #error "Unknown platform"
+
 #endif // cafe
 
 #endif // SEAD_TYPES_H_
