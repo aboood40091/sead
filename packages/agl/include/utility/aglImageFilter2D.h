c863c00f45694bcca5326eda00e847838b6ca823
AboodXD <aboood40091@gmail.com>
2023-06-13 13:07:33 +0400

    agl/utility/ImageFilter2D: Define attribute, uniform and sampler enums

--- packages/agl/include/utility/aglImageFilter2D.h
+++ packages/agl/include/utility/aglImageFilter2D.h
@@ -12,6 +12,13 @@ namespace utl {
 class ImageFilter2D
 {
 public:
+    enum Attribute
+    {
+        cAttribute_Position = 0,
+        cAttribute_Num
+    };
+    static_assert(cAttribute_Num == 1);
+
     enum Uniform
     {
         cUniform_ProjViewWorld = 0,
@@ -39,6 +46,46 @@ public:
         cUniform_Num
     };
     static_assert(cUniform_Num == 22);
+
+    enum XluSnapUniform
+    {
+        cXluSnapUniform_Alpha = cUniform_Num,
+        cXluSnapUniform_Num
+    };
+    static_assert(cXluSnapUniform_Num == 23);
+
+    enum Sampler
+    {
+        cSampler_Texture = 0,
+        cSampler_Texture_1,
+        cSampler_Num
+    };
+    static_assert(cSampler_Num == 2);
+
+    enum XluSnapSampler
+    {
+        cXluSnapSampler_SnapColor = cSampler_Num,
+        cXluSnapSampler_SnapDepth,
+        cXluSnapSampler_TargetColor,
+        cXluSnapSampler_TargetDepth,
+        cXluSnapSampler_Num
+    };
+    static_assert(cXluSnapSampler_Num == 6);
+
+    enum FrameBufferCopySampler
+    {
+        cFrameBufferCopySampler_TextureColor_0 = 0,
+        cFrameBufferCopySampler_TextureColor_1,
+        cFrameBufferCopySampler_TextureColor_2,
+        cFrameBufferCopySampler_TextureColor_3,
+        cFrameBufferCopySampler_TextureColor_4,
+        cFrameBufferCopySampler_TextureColor_5,
+        cFrameBufferCopySampler_TextureColor_6,
+        cFrameBufferCopySampler_TextureColor_7,
+        cFrameBufferCopySampler_TextureDepth,
+        cFrameBufferCopySampler_Num
+    };
+    static_assert(cFrameBufferCopySampler_Num == 9);
 
 public:
     static void drawQuadTriangle(const ShaderProgram& program, const TextureSampler& sampler);
