4d5b6266a2bf72fb5da1b454c321d5f44cd5e319
AboodXD <aboood40091@gmail.com>
2023-09-23 03:33:52 +0400

    engine/audio: Add AudioMgr

---
+++ engine/library/include/audio/seadAudioMgr.h
@@ -0,0 +1,47 @@
+#ifndef SEAD_AUDIOMGR_H_
+#define SEAD_AUDIOMGR_H_
+
+#include <container/seadOffsetList.h>
+#include <heap/seadDisposer.h>
+
+namespace sead {
+
+class AudioPlayer;
+class AudioResetter;
+class AudioResourceLoader;
+class AudioSubsetBase;
+class AudioSystem;
+class Heap;
+
+class AudioMgr
+{
+    SEAD_SINGLETON_DISPOSER(AudioMgr)
+
+public:
+    AudioMgr();
+    virtual ~AudioMgr();
+
+    AudioSystem* getAudioSystem() const
+    {
+        return mAudioSystem;
+    }
+
+protected:
+    AudioSystem* mAudioSystem;
+    AudioResetter* mResetter;
+    AudioPlayer* mPlayer;
+    AudioResourceLoader* mResourceLoader;
+    OffsetList<AudioSubsetBase> mSubsets;
+    Heap* mHeap;
+    bool mIsPrepared;
+    bool mUseDefaultAudioSystem;
+    bool mUseDefaultResetter;
+    bool mUseDefaultPlayer;
+};
+#ifdef cafe
+static_assert(sizeof(AudioMgr) == 0x3C, "sead::AudioMgr size mismatch");
+#endif // cafe
+
+} // namespace sead
+
+#endif // SEAD_AUDIOMGR_H_
