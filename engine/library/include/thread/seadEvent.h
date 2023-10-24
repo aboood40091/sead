97b0a1d9851693cea0de06e15ace0fdb01ff6505
AboodXD <aboood40091@gmail.com>
2023-10-24 18:29:50 +0400

    engine/thread: Add Event

---
+++ engine/library/include/thread/seadEvent.h
@@ -0,0 +1,42 @@
+#ifndef SEAD_EVENT_H_
+#define SEAD_EVENT_H_
+
+#ifdef cafe
+#include <cafe.h>
+#endif // cafe
+
+#include <heap/seadDisposer.h>
+
+namespace sead {
+
+class Event : public IDisposer
+{
+public:
+    Event(bool);
+
+private:
+    Event(const Event&);
+    const Event& operator=(const Event&);
+
+public:
+    virtual ~Event();
+
+    void initialize(bool);
+    void wait();
+    void setSignal();
+    void resetSignal();
+
+protected:
+#ifdef cafe
+    OSEvent mEventInner;
+#else
+    #error "Unknown platform"
+#endif // cafe
+};
+#ifdef cafe
+static_assert(sizeof(Event) == 0x34, "sead::Event size mismatch");
+#endif // cafe
+
+} // namespace sead
+
+#endif // SEAD_EVENT_H_
