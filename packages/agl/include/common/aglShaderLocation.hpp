5c7a6208eede74986e6ea3de706dc8471a9890fd
AboodXD <aboood40091@gmail.com>
2023-10-24 13:31:05 +0400

    agl/common/UniformLocation: Fix setUniform()

--- packages/agl/include/common/aglShaderLocation.hpp
+++ packages/agl/include/common/aglShaderLocation.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+// TODO: Move to the proper header
+#define SEAD_MACRO_UTIL_ROUNDUP(x, y) ((x) + ((y) - 1) & ~((y) - 1))
+
 #include <cafe/gx2.h>
 
 namespace agl {
@@ -7,11 +10,13 @@ namespace agl {
 inline void
 UniformLocation::setUniform(u32 size, const void* buffer) const
 {
+    const u32 cVec4Size = 4 * sizeof(u32);
+
     if (mVS != -1)
-        GX2SetVertexUniformReg(mVS, size / sizeof(u32), buffer);
+        GX2SetVertexUniformReg(mVS, SEAD_MACRO_UTIL_ROUNDUP(size, cVec4Size) / sizeof(u32), buffer);
 
     if (mFS != -1)
-        GX2SetPixelUniformReg(mFS, size / sizeof(u32), buffer);
+        GX2SetPixelUniformReg(mFS, SEAD_MACRO_UTIL_ROUNDUP(size, cVec4Size) / sizeof(u32), buffer);
 }
 
 }
