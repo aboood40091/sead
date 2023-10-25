52ed1131e050f8718ac48ac9f08e58fae91f01c9
AboodXD <aboood40091@gmail.com>
2023-10-25 21:39:34 +0400

    agl/environment/EnvObjMgr: Slight updates

--- packages/agl/include/environment/aglEnvObjMgr.h
+++ packages/agl/include/environment/aglEnvObjMgr.h
@@ -1,16 +1,25 @@
 #pragma once
 
+#include <environment/aglEnvObjBuffer.h>
 #include <utility/aglResParameter.h>
 
 namespace agl { namespace env {
 
-class EnvObjSet;
+// TODO
+class EnvObjSet
+{
+public:
+    u32 _0[0x1DC / sizeof(u32)];
+};
+static_assert(sizeof(EnvObjSet) == 0x1DC);
 
-class EnvObjMgr
+class EnvObjMgr : public EnvObjBuffer
 {
 public:
     EnvObjMgr();
-    /* virtual */ ~EnvObjMgr();
+    virtual ~EnvObjMgr();
+
+    // TODO: Virtual functions
 
     void applyResource(utl::ResParameterArchive arc)
     {
@@ -23,15 +32,15 @@ public:
 
     const EnvObjSet& getEnvObjSet() const
     {
-        return *(const EnvObjSet*)&mEnvObjSet;
+        return mEnvObjSet;
     }
 
 private:
     void applyResource_(utl::ResParameterArchive arc_a, utl::ResParameterArchive arc_b, f32 t);
 
 private:
-    u32 _0[0x250 / sizeof(u32)];
-    u32 mEnvObjSet[0x1DC / sizeof(u32)]; // agl::env::EnvObjSet
+    u32 _20[0x230 / sizeof(u32)];
+    EnvObjSet mEnvObjSet;
     u32 _42C[(0x464 - 0x42C) / sizeof(u32)];
 };
 static_assert(sizeof(EnvObjMgr) == 0x464, "agl::env::EnvObjMgr size mismatch");
