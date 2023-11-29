a15e86bbf2492eeb2291b9ae6fd8b48dc0f05594
AboodXD <aboood40091@gmail.com>
2023-11-29 11:48:22 +0400

    engine/gfx/GraphicsContextMRT: Complete decompilation

--- engine/library/include/gfx/seadGraphicsContextMRT.h
+++ engine/library/include/gfx/seadGraphicsContextMRT.h
@@ -44,7 +44,336 @@ public:
     void applyBlendConstantColor() const;
     void applyCullingAndPolygonModeAndPolygonOffset() const;
 
-//private:
+    void setDepthEnable(bool test_enable, bool write_enable)
+    {
+        setDepthTestEnable(test_enable);
+        setDepthWriteEnable(write_enable);
+    }
+
+    void setDepthTestEnable(bool test_enable)
+    {
+        mDepthTestEnable = test_enable;
+    }
+
+    void setDepthWriteEnable(bool write_enable)
+    {
+        mDepthWriteEnable = write_enable;
+    }
+
+    void setDepthFunc(Graphics::DepthFunc func)
+    {
+        mDepthFunc = func;
+    }
+
+    void setCullingMode(Graphics::CullingMode mode)
+    {
+        mCullingMode = mode;
+    }
+
+    void setBlendEnable(u32 target, bool blend)
+    {
+        mBlendEnableMask.changeBit(target, blend);
+    }
+
+    void setBlendFactor(u32 target, Graphics::BlendFactor src_factor, Graphics::BlendFactor dst_factor)
+    {
+        setBlendFactorSrc(target, src_factor);
+        setBlendFactorDst(target, dst_factor);
+    }
+
+    void setBlendFactorSeparate(u32 target, Graphics::BlendFactor src_factor_rgb, Graphics::BlendFactor dst_factor_rgb, Graphics::BlendFactor src_factor_a, Graphics::BlendFactor dst_factor_a)
+    {
+        setBlendFactorSrcRGB(target, src_factor_rgb);
+        setBlendFactorDstRGB(target, dst_factor_rgb);
+        setBlendFactorSrcAlpha(target, src_factor_a);
+        setBlendFactorDstAlpha(target, dst_factor_a);
+    }
+
+    void setBlendFactorSrc(u32 target, Graphics::BlendFactor factor)
+    {
+        setBlendFactorSrcRGB(target, factor);
+        setBlendFactorSrcAlpha(target, factor);
+    }
+
+    void setBlendFactorDst(u32 target, Graphics::BlendFactor factor)
+    {
+        setBlendFactorDstRGB(target, factor);
+        setBlendFactorDstAlpha(target, factor);
+    }
+
+    void setBlendFactorSrcRGB(u32 target, Graphics::BlendFactor factor)
+    {
+        mBlendExpression[target].blend_factor_src_rgb = factor;
+    }
+
+    void setBlendFactorSrcAlpha(u32 target, Graphics::BlendFactor factor)
+    {
+        mBlendExpression[target].blend_factor_src_a = factor;
+    }
+
+    void setBlendFactorDstRGB(u32 target, Graphics::BlendFactor factor)
+    {
+        mBlendExpression[target].blend_factor_dst_rgb = factor;
+    }
+
+    void setBlendFactorDstAlpha(u32 target, Graphics::BlendFactor factor)
+    {
+        mBlendExpression[target].blend_factor_dst_a = factor;
+    }
+
+    void setBlendEquation(u32 target, Graphics::BlendEquation equation)
+    {
+        setBlendEquationRGB(target, equation);
+        setBlendEquationAlpha(target, equation);
+    }
+
+    void setBlendEquationSeparate(u32 target, Graphics::BlendEquation equation_rgb, Graphics::BlendEquation equation_a)
+    {
+        setBlendEquationRGB(target, equation_rgb);
+        setBlendEquationAlpha(target, equation_a);
+    }
+
+    void setBlendEquationRGB(u32 target, Graphics::BlendEquation equation)
+    {
+        mBlendExpression[target].blend_equation_rgb = equation;
+    }
+
+    void setBlendEquationAlpha(u32 target, Graphics::BlendEquation equation)
+    {
+        mBlendExpression[target].blend_equation_a = equation;
+    }
+
+    void setBlendConstantColor(const Color4f& color)
+    {
+        mBlendConstantColor = color;
+    }
+
+    void setAlphaTestEnable(bool enable)
+    {
+        mAlphaTestEnable = enable;
+    }
+
+    void setAlphaTestFunc(Graphics::AlphaFunc func, f32 ref)
+    {
+        mAlphaTestFunc = func;
+        mAlphaTestRef = ref;
+    }
+
+    void setColorMask(u32 target, bool r, bool g, bool b, bool a)
+    {
+        u32 color_mask = r << 0 |
+                         g << 1 |
+                         b << 2 |
+                         a << 3;
+        mColorMask = (mColorMask & ~(0xF << (target * 4))) | (color_mask << (target * 4));
+    }
+
+    void setStencilTestEnable(bool enable)
+    {
+        mStencilTestEnable = enable;
+    }
+
+    void setStencilTestFunc(Graphics::StencilFunc func, s32 ref, u32 mask)
+    {
+        mStencilTestFunc = func;
+        mStencilTestRef = ref;
+        mStencilTestMask = mask;
+    }
+
+    void setStencilTestOp(Graphics::StencilOp fail, Graphics::StencilOp zfail, Graphics::StencilOp zpass)
+    {
+        mStencilOpFail = fail;
+        mStencilOpZFail = zfail;
+        mStencilOpZPass = zpass;
+    }
+
+    void setPolygonMode(Graphics::PolygonMode front, Graphics::PolygonMode back)
+    {
+#ifdef cafe
+        mPolygonModeFront = front;
+        mPolygonModeBack = back;
+#endif // cafe
+    }
+
+    void setPolygonOffsetEnable(bool fill_front_enable, bool fill_back_enable, bool point_line_enable)
+    {
+#ifdef cafe
+        mPolygonOffsetFrontEnable = fill_front_enable;
+        mPolygonOffsetBackEnable = fill_back_enable;
+        mPolygonOffsetPointLineEnable = point_line_enable;
+#endif // cafe
+    }
+
+    bool getDepthTestEnable() const
+    {
+        return mDepthTestEnable;
+    }
+
+    bool getDepthWriteEnable() const
+    {
+        return mDepthWriteEnable;
+    }
+
+    Graphics::DepthFunc getDepthFunc() const
+    {
+        return mDepthFunc;
+    }
+
+    Graphics::CullingMode getCullingMode() const
+    {
+        return mCullingMode;
+    }
+
+    bool getBlendEnable(u32 target) const
+    {
+        return mBlendEnableMask.isOnBit(target);
+    }
+
+    Graphics::BlendFactor getBlendFactorSrcRGB(u32 target) const
+    {
+        return mBlendExpression[target].blend_factor_src_rgb;
+    }
+
+    Graphics::BlendFactor getBlendFactorSrcAlpha(u32 target) const
+    {
+        return mBlendExpression[target].blend_factor_src_a;
+    }
+
+    Graphics::BlendFactor getBlendFactorDstRGB(u32 target) const
+    {
+        return mBlendExpression[target].blend_factor_dst_rgb;
+    }
+
+    Graphics::BlendFactor getBlendFactorDstAlpha(u32 target) const
+    {
+        return mBlendExpression[target].blend_factor_dst_a;
+    }
+
+    Graphics::BlendEquation getBlendEquationRGB(u32 target) const
+    {
+        return mBlendExpression[target].blend_equation_rgb;
+    }
+
+    Graphics::BlendEquation getBlendEquationAlpha(u32 target) const
+    {
+        return mBlendExpression[target].blend_equation_a;
+    }
+
+    const Color4f& getBlendConstantColor() const
+    {
+        return mBlendConstantColor;
+    }
+
+    bool getAlphaTestEnable() const
+    {
+        return mAlphaTestEnable;
+    }
+
+    Graphics::AlphaFunc getAlphaTestFunc() const
+    {
+        return mAlphaTestFunc;
+    }
+
+    f32 getAlphaTestRef() const
+    {
+        return mAlphaTestRef;
+    }
+
+    bool getColorMaskR(u32 target) const
+    {
+        u32 color_mask = mColorMask >> (target * 4) & 0xF;
+        return color_mask >> 0 & 1;
+    }
+
+    bool getColorMaskG(u32 target) const
+    {
+        u32 color_mask = mColorMask >> (target * 4) & 0xF;
+        return color_mask >> 1 & 1;
+    }
+
+    bool getColorMaskB(u32 target) const
+    {
+        u32 color_mask = mColorMask >> (target * 4) & 0xF;
+        return color_mask >> 2 & 1;
+    }
+
+    bool getColorMaskA(u32 target) const
+    {
+        u32 color_mask = mColorMask >> (target * 4) & 0xF;
+        return color_mask >> 3 & 1;
+    }
+
+    bool getStencilTestEnable() const
+    {
+        return mStencilTestEnable;
+    }
+
+    Graphics::StencilFunc getStencilTestFunc() const
+    {
+        return mStencilTestFunc;
+    }
+
+    s32 getStencilTestRef() const
+    {
+        return mStencilTestRef;
+    }
+
+    u32 getStencilTestMask() const
+    {
+        return mStencilTestMask;
+    }
+
+    Graphics::StencilOp getStencilTestOpFail() const
+    {
+        return mStencilOpFail;
+    }
+
+    Graphics::StencilOp getStencilTestOpZFail() const
+    {
+        return mStencilOpZFail;
+    }
+
+    Graphics::StencilOp getStencilTestOpZPass() const
+    {
+        return mStencilOpZPass;
+    }
+
+    Graphics::PolygonMode getPolygonModeFront() const
+    {
+#ifdef cafe
+        return mPolygonModeFront;
+#endif // cafe
+    }
+
+    Graphics::PolygonMode getPolygonModeBack() const
+    {
+#ifdef cafe
+        return mPolygonModeBack;
+#endif // cafe
+    }
+
+    bool getPolygonOffsetFrontEnable() const
+    {
+#ifdef cafe
+        return mPolygonOffsetFrontEnable;
+#endif // cafe
+    }
+
+    bool getPolygonOffsetBackEnable() const
+    {
+#ifdef cafe
+        return mPolygonOffsetBackEnable;
+#endif // cafe
+    }
+
+    bool getPolygonOffsetPointLineEnable() const
+    {
+#ifdef cafe
+        return mPolygonOffsetPointLineEnable;
+#endif // cafe
+    }
+
+private:
     bool mDepthTestEnable;
     bool mDepthWriteEnable;
     Graphics::DepthFunc mDepthFunc;
--- engine/library/modules/src/gfx/seadGraphicsContextMRT.cpp
+++ engine/library/modules/src/gfx/seadGraphicsContextMRT.cpp
@@ -6,13 +6,18 @@ GraphicsContextMRT::GraphicsContextMRT()
     : mDepthTestEnable(true)
     , mDepthWriteEnable(true)
     , mDepthFunc(Graphics::cDepthFunc_LessEqual)
+
     , mCullingMode(Graphics::cCullingMode_Back)
+
     , mBlendEnableMask(0xFF)
     , mBlendConstantColor(1.0f, 1.0f, 1.0f, 1.0f)
+
     , mAlphaTestEnable(false)
     , mAlphaTestFunc(Graphics::cAlphaFunc_Greater)
     , mAlphaTestRef(0.0f)
+
     , mColorMask(0xFFFFFFFF)
+
     , mStencilTestEnable(false)
     , mStencilTestFunc(Graphics::cStencilFunc_Never)
     , mStencilTestRef(0)
@@ -20,6 +25,7 @@ GraphicsContextMRT::GraphicsContextMRT()
     , mStencilOpFail(Graphics::cStencilOp_Keep)
     , mStencilOpZFail(Graphics::cStencilOp_Keep)
     , mStencilOpZPass(Graphics::cStencilOp_Keep)
+
 #ifdef cafe
     , mPolygonModeFront(Graphics::cPolygonMode_Fill)
     , mPolygonModeBack(Graphics::cPolygonMode_Fill)
@@ -28,6 +34,205 @@ GraphicsContextMRT::GraphicsContextMRT()
     , mPolygonOffsetPointLineEnable(false)
 #endif // cafe
 {
+}
+
+void GraphicsContextMRT::apply() const
+{
+#ifdef cafe
+    GX2SetDepthStencilControl(
+        static_cast<GX2Boolean>(mDepthTestEnable),
+        static_cast<GX2Boolean>(mDepthWriteEnable),
+        static_cast<GX2CompareFunction>(mDepthFunc),
+        static_cast<GX2Boolean>(mStencilTestEnable),
+        static_cast<GX2Boolean>(mStencilTestEnable),
+        static_cast<GX2CompareFunction>(mStencilTestFunc),
+        static_cast<GX2StencilFunction>(mStencilOpZPass),
+        static_cast<GX2StencilFunction>(mStencilOpZFail),
+        static_cast<GX2StencilFunction>(mStencilOpFail),
+        static_cast<GX2CompareFunction>(mStencilTestFunc),
+        static_cast<GX2StencilFunction>(mStencilOpZPass),
+        static_cast<GX2StencilFunction>(mStencilOpZFail),
+        static_cast<GX2StencilFunction>(mStencilOpFail)
+    );
+
+    GX2SetStencilMask(mStencilTestMask, mStencilTestRef, mStencilTestRef,  // Bug: ref in place of write mask
+                      mStencilTestMask, mStencilTestRef, mStencilTestRef); // ^^^
+
+    {
+        static const GX2Boolean cCullSettings[4][2] = {
+            { GX2_ENABLE, GX2_DISABLE },
+            { GX2_DISABLE, GX2_ENABLE },
+            { GX2_DISABLE, GX2_DISABLE },
+            { GX2_ENABLE, GX2_ENABLE }
+        };
+        const GX2Boolean* const& settings = cCullSettings[mCullingMode];
+
+        GX2SetPolygonControl(
+            GX2_FRONT_FACE_CCW,
+            settings[0],
+            settings[1],
+            static_cast<GX2Boolean>(mPolygonModeFront != Graphics::cPolygonMode_Fill || mPolygonModeBack != Graphics::cPolygonMode_Fill),
+            static_cast<GX2PolygonMode>(mPolygonModeFront),
+            static_cast<GX2PolygonMode>(mPolygonModeBack),
+            static_cast<GX2Boolean>(mPolygonOffsetFrontEnable),
+            static_cast<GX2Boolean>(mPolygonOffsetBackEnable),
+            static_cast<GX2Boolean>(mPolygonOffsetPointLineEnable)
+        );
+    }
+
+    GX2SetColorControl(
+        GX2_LOGIC_OP_NONE,
+        mBlendEnableMask.getDirect() & 0xFF,
+        GX2_DISABLE,
+        static_cast<GX2Boolean>(mColorMask != 0)
+    );
+
+    for (SafeArray<BlendExpression, Graphics::cRenderTarget_Num>::constIterator itr = mBlendExpression.constBegin(), itr_end = mBlendExpression.constEnd(); itr != itr_end; ++itr)
+    {
+        GX2SetBlendControl(
+            static_cast<GX2RenderTarget>(GX2_RENDER_TARGET_0 + itr.getIndex()),
+            static_cast<GX2BlendFunction>(itr->blend_factor_src_rgb),
+            static_cast<GX2BlendFunction>(itr->blend_factor_dst_rgb),
+            static_cast<GX2BlendCombine>(itr->blend_equation_rgb),
+            GX2_ENABLE,
+            static_cast<GX2BlendFunction>(itr->blend_factor_src_a),
+            static_cast<GX2BlendFunction>(itr->blend_factor_dst_a),
+            static_cast<GX2BlendCombine>(itr->blend_equation_a)
+        );
+    }
+
+    GX2SetBlendConstantColor(mBlendConstantColor.r,
+                             mBlendConstantColor.g,
+                             mBlendConstantColor.b,
+                             mBlendConstantColor.a);
+
+    GX2SetAlphaTest(static_cast<GX2Boolean>(mAlphaTestEnable),
+                    static_cast<GX2CompareFunction>(mAlphaTestFunc),
+                    mAlphaTestRef);
+
+    GX2SetTargetChannelMasks(
+        static_cast<GX2ChannelMask>(mColorMask >>  0 & 0xF),
+        static_cast<GX2ChannelMask>(mColorMask >>  4 & 0xF),
+        static_cast<GX2ChannelMask>(mColorMask >>  8 & 0xF),
+        static_cast<GX2ChannelMask>(mColorMask >> 12 & 0xF),
+        static_cast<GX2ChannelMask>(mColorMask >> 16 & 0xF),
+        static_cast<GX2ChannelMask>(mColorMask >> 20 & 0xF),
+        static_cast<GX2ChannelMask>(mColorMask >> 24 & 0xF),
+        static_cast<GX2ChannelMask>(mColorMask >> 28 & 0xF)
+    );
+
+    GX2SetAlphaToMask(GX2_DISABLE, GX2_ALPHA_TO_MASK_0);
+#endif // cafe
+}
+
+void GraphicsContextMRT::applyAlphaTest() const
+{
+#ifdef cafe
+    GX2SetAlphaTest(static_cast<GX2Boolean>(mAlphaTestEnable),
+                    static_cast<GX2CompareFunction>(mAlphaTestFunc),
+                    mAlphaTestRef);
+#endif // cafe
+}
+
+void GraphicsContextMRT::applyDepthAndStencilTest() const
+{
+#ifdef cafe
+    GX2SetDepthStencilControl(
+        static_cast<GX2Boolean>(mDepthTestEnable),
+        static_cast<GX2Boolean>(mDepthWriteEnable),
+        static_cast<GX2CompareFunction>(mDepthFunc),
+        static_cast<GX2Boolean>(mStencilTestEnable),
+        static_cast<GX2Boolean>(mStencilTestEnable),
+        static_cast<GX2CompareFunction>(mStencilTestFunc),
+        static_cast<GX2StencilFunction>(mStencilOpZPass),
+        static_cast<GX2StencilFunction>(mStencilOpZFail),
+        static_cast<GX2StencilFunction>(mStencilOpFail),
+        static_cast<GX2CompareFunction>(mStencilTestFunc),
+        static_cast<GX2StencilFunction>(mStencilOpZPass),
+        static_cast<GX2StencilFunction>(mStencilOpZFail),
+        static_cast<GX2StencilFunction>(mStencilOpFail)
+    );
+
+    GX2SetStencilMask(mStencilTestMask, mStencilTestRef, mStencilTestRef,  // Bug: ref in place of write mask
+                      mStencilTestMask, mStencilTestRef, mStencilTestRef); // ^^^
+#endif // cafe
+}
+
+void GraphicsContextMRT::applyColorMask() const
+{
+#ifdef cafe
+    GX2SetTargetChannelMasks(
+        static_cast<GX2ChannelMask>(mColorMask >>  0 & 0xF),
+        static_cast<GX2ChannelMask>(mColorMask >>  4 & 0xF),
+        static_cast<GX2ChannelMask>(mColorMask >>  8 & 0xF),
+        static_cast<GX2ChannelMask>(mColorMask >> 12 & 0xF),
+        static_cast<GX2ChannelMask>(mColorMask >> 16 & 0xF),
+        static_cast<GX2ChannelMask>(mColorMask >> 20 & 0xF),
+        static_cast<GX2ChannelMask>(mColorMask >> 24 & 0xF),
+        static_cast<GX2ChannelMask>(mColorMask >> 28 & 0xF)
+    );
+#endif // cafe
+}
+
+void GraphicsContextMRT::applyBlendAndFastZ() const
+{
+#ifdef cafe
+    GX2SetColorControl(
+        GX2_LOGIC_OP_NONE,
+        mBlendEnableMask.getDirect() & 0xFF,
+        GX2_DISABLE,
+        static_cast<GX2Boolean>(mColorMask != 0)
+    );
+
+    for (SafeArray<BlendExpression, Graphics::cRenderTarget_Num>::constIterator itr = mBlendExpression.constBegin(), itr_end = mBlendExpression.constEnd(); itr != itr_end; ++itr)
+    {
+        GX2SetBlendControl(
+            static_cast<GX2RenderTarget>(GX2_RENDER_TARGET_0 + itr.getIndex()),
+            static_cast<GX2BlendFunction>(itr->blend_factor_src_rgb),
+            static_cast<GX2BlendFunction>(itr->blend_factor_dst_rgb),
+            static_cast<GX2BlendCombine>(itr->blend_equation_rgb),
+            GX2_ENABLE,
+            static_cast<GX2BlendFunction>(itr->blend_factor_src_a),
+            static_cast<GX2BlendFunction>(itr->blend_factor_dst_a),
+            static_cast<GX2BlendCombine>(itr->blend_equation_a)
+        );
+    }
+#endif // cafe
+}
+
+void GraphicsContextMRT::applyBlendConstantColor() const
+{
+#ifdef cafe
+    GX2SetBlendConstantColor(mBlendConstantColor.r,
+                             mBlendConstantColor.g,
+                             mBlendConstantColor.b,
+                             mBlendConstantColor.a);
+#endif // cafe
+}
+
+void GraphicsContextMRT::applyCullingAndPolygonModeAndPolygonOffset() const
+{
+#ifdef cafe
+    static const GX2Boolean cCullSettings[4][2] = {
+        { GX2_ENABLE, GX2_DISABLE },
+        { GX2_DISABLE, GX2_ENABLE },
+        { GX2_DISABLE, GX2_DISABLE },
+        { GX2_ENABLE, GX2_ENABLE }
+    };
+    const GX2Boolean* const& settings = cCullSettings[mCullingMode];
+
+    GX2SetPolygonControl(
+        GX2_FRONT_FACE_CCW,
+        settings[0],
+        settings[1],
+        static_cast<GX2Boolean>(mPolygonModeFront != Graphics::cPolygonMode_Fill || mPolygonModeBack != Graphics::cPolygonMode_Fill),
+        static_cast<GX2PolygonMode>(mPolygonModeFront),
+        static_cast<GX2PolygonMode>(mPolygonModeBack),
+        static_cast<GX2Boolean>(mPolygonOffsetFrontEnable),
+        static_cast<GX2Boolean>(mPolygonOffsetBackEnable),
+        static_cast<GX2Boolean>(mPolygonOffsetPointLineEnable)
+    );
+#endif // cafe
 }
 
 } // namespace sead
