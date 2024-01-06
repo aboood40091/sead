615657fcfdd9307a8ae5fac43baa0194ca18a69a
AboodXD <aboood40091@gmail.com>
2024-01-06 22:02:34 +0400

    agl/utility/INamedObj: Implement virtual functions

--- packages/agl/include/utility/aglNamedObj.h
+++ packages/agl/include/utility/aglNamedObj.h
@@ -10,10 +10,10 @@ public:
     INamedObj();
     virtual ~INamedObj();
 
-    virtual const sead::SafeString& getObjName() const;
-    virtual const sead::SafeString& getGroupName() const;
-    virtual s32 getObjType() const;
-    virtual bool isHostIOEnabled() const;
+    virtual const sead::SafeString& getObjName() const { return sead::SafeString::cEmptyString; }
+    virtual const sead::SafeString& getGroupName() const { return sead::SafeString::cEmptyString; }
+    virtual s32 getObjType() const { return 0; }
+    virtual bool isHostIOEnabled() const { return true; }
 };
 static_assert(sizeof(INamedObj) == 4, "agl::utl::INamedObj size mismatch");
 
