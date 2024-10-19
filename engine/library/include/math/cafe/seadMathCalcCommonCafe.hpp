5a1d78a0d20cd22f3e65f5cfef9b622cb9984588
AboodXD <aboood40091@gmail.com>
2024-10-19 19:20:39 +0400

    engine/math/cafe/MathCafe: Add intrinsic-based implementation for rsqrt

--- engine/library/include/math/cafe/seadMathCalcCommonCafe.hpp
+++ engine/library/include/math/cafe/seadMathCalcCommonCafe.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <ppc_ghs.h>
+
 namespace sead {
 
 template <>
@@ -16,6 +18,17 @@ inline f32 MathCafe<f32>::rsqrt(f32 register x)
     const f32 register HALF  = 0.5f;
     const f32 register THREE = 3.0f;
 
+#ifdef SEAD_MATH_CAFE_RSQRT_USE_INTRINSICS
+    // The use of inline assembly in an inline function can be very dangerous,
+    // and sometimes triggers compiler bugs in GHS.
+    // Therefore, use intrinsics instead.
+    // (For use with custom applications)
+    v0 = __FRSQRTE(x);
+    v1 = v0 * v0;
+    v2 = v0 * HALF;
+    v1 = __FNMSUBS(v1, x, THREE);
+    v0 = v1 * v2;
+#else
     asm ("frsqrte %[v0], %[x]                 \n\t"
          "fmuls   %[v1], %[v0], %[v0]         \n\t"
          "fmuls   %[v2], %[v0], %[HALF]       \n\t"
@@ -23,6 +36,7 @@ inline f32 MathCafe<f32>::rsqrt(f32 register x)
          "fmuls   %[v0], %[v1], %[v2]             "
          : [v0] "+f"(v0), [  v1] "+f"(  v1), [   v2] "+f"(   v2)
          : [ x]  "f"( x), [HALF]  "f"(HALF), [THREE]  "f"(THREE));
+#endif
 
     return v0;
 }
