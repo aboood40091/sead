844fc59790b6e54b974479891efecf0a7552af77
AboodXD <aboood40091@gmail.com>
2025-01-01 13:10:46 +0400

    engine/container/ListImpl: Implement mergeSortImpl()
    100% matching.

--- engine/library/modules/src/container/seadListImpl.cpp
+++ engine/library/modules/src/container/seadListImpl.cpp
@@ -88,4 +88,90 @@ void ListImpl::clear()
     mStartEnd.mPrev = &mStartEnd;
 }
 
+void ListImpl::mergeSortImpl(ListNode* front, ListNode* back, s32 num, s32 offset, CompareCallbackImpl cmp)
+{
+    if (num > 8)
+    {
+        s32 front_count;
+        ListNode* middle;
+        s32 back_count;
+        ListNode* merge_top;
+        ListNode* back_top;
+        bool take_from_front;
+
+        front_count = num >> 1;
+
+        middle = front;
+        for (s32 i = 1; i < front_count; i++)
+            middle = middle->next();
+
+        back_count = num - front_count;
+
+        merge_top = front->prev();
+        back_top = middle->next();
+        mergeSortImpl(front, middle, front_count, offset, cmp);
+        front = merge_top->next();
+        middle = back_top->prev();
+        mergeSortImpl(back_top, back, back_count, offset, cmp);
+        back_top = middle->next();
+
+        while (front_count > 0 || back_count > 0)
+        {
+            if (front_count == 0)
+                take_from_front = false;
+            else if (back_count == 0)
+                take_from_front = true;
+            else if (cmp((u8*)front - offset, (u8*)back_top - offset) > 0)
+                take_from_front = false;
+            else
+                take_from_front = true;
+
+            if (take_from_front)
+            {
+                ListNode* temp = front->next();
+                front->erase_();
+                merge_top->insertBack_(front);
+                merge_top = front;
+                front = temp;
+                front_count--;
+            }
+            else
+            {
+                ListNode* temp = back_top->next();
+                back_top->erase_();
+                merge_top->insertBack_(back_top);
+                merge_top = back_top;
+                back_top = temp;
+                back_count--;
+            }
+        }
+    }
+    else if (num >= 2)
+    {
+        ListNode* start = front->prev();
+        ListNode* node = front->next();
+        ListNode* end = back->next();
+
+        while (node != end)
+        {
+            ListNode* next = node->mNext;
+            ListNode* prev = node->mPrev;
+
+            if (cmp((u8*)prev - offset, (u8*)node - offset) > 0)
+            {
+                while ((prev = prev->mPrev) != start)
+                {
+                    if (cmp((u8*)prev - offset, (u8*)node - offset) <= 0)
+                        break;
+                }
+
+                node->erase_();
+                prev->insertBack_(node);
+            }
+
+            node = next;
+        }
+    }
+}
+
 } // namespace sead
