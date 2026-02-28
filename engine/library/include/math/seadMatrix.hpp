00bdcaf15aa243731fab5b639f6f6a5e4a1b4ff8
AboodXD <aboood40091@gmail.com>
2026-02-28 09:46:31 +0400

    engine/math/seadMatrix.hpp: Remove extra semicolon

--- engine/library/include/math/seadMatrix.hpp
+++ engine/library/include/math/seadMatrix.hpp
@@ -848,7 +848,7 @@ template <typename T>
 inline void
 Matrix44<T>::makeIdentity()
 {
-    Matrix44CalcCommon<T>::makeIdentity(*this);;
+    Matrix44CalcCommon<T>::makeIdentity(*this);
 }
 
 template <typename T>
