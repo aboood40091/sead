3efd0db3d13477a416a23cbdff04a86601a6d97e
AboodXD <aboood40091@gmail.com>
2026-06-27 12:08:56 +0400

    engine/framework/InfLoopCheckerThread: Add temp definition

---
+++ engine/library/include/framework/seadInfLoopCheckerThread.h
@@ -0,0 +1,16 @@
+#ifndef SEAD_INF_LOOP_CHECKER_THREAD_H_
+#define SEAD_INF_LOOP_CHECKER_THREAD_H_
+
+#include <thread/seadThread.h>
+
+namespace sead {
+
+class InfLoopCheckerThread : public Thread
+{
+public:
+    // TODO
+};
+
+} // namespace sead
+
+#endif // SEAD_INF_LOOP_CHECKER_THREAD_H_
