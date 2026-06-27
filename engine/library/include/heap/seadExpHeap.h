b75a2f19a735092ce32f0ef9c871b96ea2af26ac
AboodXD <aboood40091@gmail.com>
2026-06-27 12:16:57 +0400

    engine/heap/ExpHeap: Add create

--- engine/library/include/heap/seadExpHeap.h
+++ engine/library/include/heap/seadExpHeap.h
@@ -19,6 +19,16 @@ public:
 
 public:
     static ExpHeap* tryCreate(u32 size, const SafeString& name, Heap* parent = nullptr, HeapDirection direction = cHeapDirection_Forward, bool enable_lock = false);
+
+    static ExpHeap* create(u32 size, const SafeString& name, Heap* parent = nullptr, HeapDirection direction = cHeapDirection_Forward, bool enable_lock = false)
+    {
+        ExpHeap* heap = tryCreate(size, name, parent, direction, enable_lock);
+        if (!heap)
+        {
+            // SEAD_ASSERT_MSG(false, "heap create failed. [%s] size: %d, parent: %s(%p)", name.cstr(), size, parent ? parent->getName().cstr() : "--", parent);
+        }
+        return heap;
+    }
 };
 
 } // namespace sead
