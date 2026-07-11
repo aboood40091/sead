0468fda737ea9c9ef4f81687a0cc2581f05b5aa1
AboodXD <aboood40091@gmail.com>
2026-07-11 13:30:20 +0400

    engine/container/ListImpl: Implement a few more functions

--- engine/library/modules/src/container/seadListImpl.cpp
+++ engine/library/modules/src/container/seadListImpl.cpp
@@ -1,4 +1,5 @@
 #include <container/seadListImpl.h>
+#include <random/seadRandom.h>
 
 namespace sead {
 
@@ -44,6 +45,81 @@ void ListNode::erase_()
     mPrev = nullptr;
 }
 
+void ListImpl::swap(ListNode* n1, ListNode* n2)
+{
+    //SEAD_ASSERT(n1->mPrev && n1->mNext && n2->mPrev && n2->mNext);
+    if (n1 != n2)
+    {
+        ListNode* n1_prev = n1->mPrev;
+        ListNode* n2_prev = n2->mPrev;
+        if (n2_prev != n1)
+        {
+            n1->erase_();
+            n2_prev->insertBack_(n1);
+        }
+        if (n1_prev != n2)
+        {
+            n2->erase_();
+            n1_prev->insertBack_(n2);
+        }
+    }
+}
+
+ListNode* ListImpl::nth(s32 index) const
+{
+    if ((u32)index < (u32)mCount)
+    {
+        ListNode* node = mStartEnd.mNext;
+        for (s32 i = 0; i < index; i++)
+            node = node->mNext;
+        return node;
+    }
+    else
+    {
+        //SEAD_ASSERT_MSG(false, "index exceeded[%d/%d]", index, mCount);
+        return nullptr;
+    }
+}
+
+void ListImpl::shuffle(Random* random)
+{
+    //SEAD_ASSERT(random);
+    s32 count = mCount;
+    while (count > 1)
+    {
+        s32 index = random->getS32Range(0, count);
+        count--;
+        swap(nth(count), nth(index));
+    }
+}
+
+void ListImpl::sort(s32 offset, CompareCallbackImpl cmp)
+{
+    if (mCount > 1)
+    {
+        ListNode* node = nth(1);
+        while (node != &mStartEnd)
+        {
+            ListNode* node_next = node->mNext;
+            ListNode* node_prev = node->mPrev;
+
+            if (cmp((u8*)node_prev - offset, (u8*)node - offset) > 0)
+            {
+                while ((node_prev = node_prev->mPrev) != &mStartEnd)
+                {
+                    if (!(cmp((u8*)node_prev - offset, (u8*)node - offset) > 0))
+                        break;
+                }
+
+                node->erase_();
+                node_prev->insertBack_(node);
+            }
+
+            node = node_next;
+        }
+    }
+}
+
 ListNode* ListImpl::popBack()
 {
     if (mCount > 0)
@@ -72,6 +148,56 @@ ListNode* ListImpl::popFront()
     {
         return nullptr;
     }
+}
+
+s32 ListImpl::indexOf(const ListNode* n) const
+{
+    ListNode* node = mStartEnd.mNext;
+    s32 index = 0;
+    while (node != &mStartEnd)
+    {
+        if (node == n)
+            return index;
+
+        node = node->mNext;
+        index++;
+    }
+
+    return -1;
+}
+
+void ListImpl::moveAfter(ListNode* basis, ListNode* n)
+{
+    //SEAD_ASSERT(basis->mPrev && basis->mNext && n->mPrev && n->mNext);
+    if (basis != n)
+    {
+        n->erase_();
+        basis->insertBack_(n);
+    }
+}
+
+void ListImpl::moveBefore(ListNode* basis, ListNode* n)
+{
+    //SEAD_ASSERT(basis->mPrev && basis->mNext && n->mPrev && n->mNext);
+    if (basis != n)
+    {
+        n->erase_();
+        basis->insertFront_(n);
+    }
+}
+
+ListNode* ListImpl::find(const void* ptr, s32 offset, CompareCallbackImpl cmp) const
+{
+    ListNode* node = mStartEnd.mNext;
+    while (node != &mStartEnd)
+    {
+        if (cmp((u8*)node - offset, ptr) == 0)
+            return node;
+
+        node = node->mNext;
+    }
+
+    return nullptr;
 }
 
 void ListImpl::clear()
